9e863efc0f0eeb7be64d218517373980f3d8018d
budgetdevv <thetrumpmcdonaldz@gmail.com>
2026-03-15 04:39:28 +0800

    Updated test code

--- MathStuff/Program.cs
+++ MathStuff/Program.cs
@@ -4,15 +4,29 @@ namespace MathStuff
 {
     internal static class Program
     {
+        private static readonly Vertex VERTEX_2 = 2;
+
         private static void Main(string[] args)
         {
+            var vertex2 = VERTEX_2;
+
             var graph = new Graph(
-                vertices: [ 1, 2, 3 ],
-                edges: [ (1, 2), (2, 3) ],
+                vertices: [ 1, vertex2, 3 ],
+                edges: [ (1, vertex2), (vertex2, 3) ],
                 isDirectedGraph: false
             );
 
-            Console.WriteLine(graph);
+            var graphPrintText =
+            $"""
+            Graph data:
+            
+            {graph}
+
+            """;
+
+            Console.WriteLine(graphPrintText);
+
+            Console.WriteLine($"Degree of vertex {vertex2} - {graph.GetVertexData(vertex2).Degree}");
         }
     }
 }
